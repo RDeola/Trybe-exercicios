6ebbc43e4166fb3ddf7774e7db5d46d25ed878f5
Rubens Deola <rdeola@gmail.com>
2022-05-19 16:25:24 -0300

    exercicios da parte II

---
+++ 1-fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/parte-II.js
@@ -0,0 +1,16 @@
+document.getElementById("page-title").innerText = 'Alterado';
+const secParagraph = document.getElementById("second-paragraph");
+  secParagraph.style.backgroundColor = 'blue';
+
+const subTitle = document.getElementById("subtitle");
+  subTitle.style.backgroundColor = 'green';
+  subTitle.style.textAlign = 'center';
+
+const classParagraph = document.getElementsByClassName("par");
+  classParagraph[1].style.color = 'pink';
+
+const tagParagraph = document.getElementsByTagName("p");
+  for (i = 0; i < tagParagraph.length; i += 1) {
+    tagParagraph[i].innerText = `${i + 1}  - ${tagParagraph[i].innerText}`;
+  }
+
