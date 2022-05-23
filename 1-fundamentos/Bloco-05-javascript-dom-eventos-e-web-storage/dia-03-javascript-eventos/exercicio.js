279b445488f13660c30db1b8996d6cd033104bfb
Rubens Deola <rdeola@gmail.com>
2022-05-23 11:03:29 -0300

    exercicio 2 Exercicios

--- 1-fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicio.js
+++ 1-fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicio.js
@@ -1,3 +1,5 @@
+const buttonsContainer = document.querySelector('.buttons-container');
+
 function createDaysOfTheWeek() {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
   const weekDaysList = document.querySelector('.week-days');
@@ -14,6 +16,7 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+//Exercício 1
 // O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days". Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
 // - Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex: <li class="day">3</li>
 // - Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li>
@@ -42,3 +45,19 @@ function populaDays() {
   }
 }
 populaDays();
+
+//Exercicio 2
+// Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+//  - Adicione a este botão a ID "btn-holiday".
+//  - Adicione este botão como filho/filha da tag <div> com classe "buttons-container".
+function createButton(parent, string) {
+  const tagButton = document.createElement('button');
+  tagButton.innerHTML = string;
+  tagButton.id = 'btn-holiday';
+  parent.appendChild(tagButton);
+}
+createButton(buttonsContainer, 'Feriados');
+
+//Exercicio 3
+// Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday".
+//  - É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)".
