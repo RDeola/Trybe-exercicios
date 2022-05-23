7b63d1a497349b8723678ef3006ef258035ed242
Rubens Deola <rdeola@gmail.com>
2022-05-23 15:35:54 -0300

    exercicios 9 finalizados em Exercicios

--- 1-fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicio.js
+++ 1-fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicio.js
@@ -132,7 +132,7 @@ function newTask(task) {
 }
 newTask('Pular');
 
-// Exercício 8:
+// Exercício 8
 // Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task.
 // O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
 // O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
@@ -143,3 +143,18 @@ function newLegenda(cor) {
   tagMyTasks.appendChild(tagDiv);
 }
 newLegenda('yellow');
+
+// Exercício 9
+// Implemente uma função que adiciona um evento que, ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected, ou seja, quando sua tarefa possuir a classe task selected, ela estará selecionada.
+// Ao clicar novamente no elemento, a sua classe deverá voltar a ser somente task, ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
+tagMyTasks.addEventListener('click', function(element) {
+  if (element.target.classList.contains('task')) {
+    console.log(element.target.classList.contains('selected'));
+    if (element.target.classList.contains('selected')) {
+      element.target.classList.remove('selected');
+    } else {
+      element.target.classList.add('selected');
+    }
+  }
+});
+
