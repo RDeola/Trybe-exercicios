4c5f841265c618170aef62264cbeeee2fba4c97f
Rubens Deola <rdeola@gmail.com>
2022-05-23 12:46:10 -0300

    exercicio 3 Exercicios

--- 1-fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicio.js
+++ 1-fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicio.js
@@ -1,7 +1,15 @@
 const buttonsContainer = document.querySelector('.buttons-container');
 
 function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDays = [
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado',
+  ];
   const weekDaysList = document.querySelector('.week-days');
 
   for (let index = 0; index < weekDays.length; index += 1) {
@@ -10,8 +18,8 @@ function createDaysOfTheWeek() {
     dayListItem.innerHTML = days;
 
     weekDaysList.appendChild(dayListItem);
-  };
-};
+  }
+}
 
 createDaysOfTheWeek();
 
@@ -21,21 +29,24 @@ createDaysOfTheWeek();
 // - Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex: <li class="day">3</li>
 // - Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li>
 // - Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li>
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
 const dezHolidays = [24, 25, 31];
 const dezFridays = [4, 11, 18, 25];
 
 function populaDays() {
   const ulDays = document.getElementById('days');
-  for ( i = 0; i < dezDaysList.length; i += 1) {
+  for (i = 0; i < dezDaysList.length; i += 1) {
     const liDay = document.createElement('li');
     liDay.classList.add('day');
 
-    let filtrado = dezHolidays.filter(valor => (valor === dezDaysList[i]));
+    let filtrado = dezHolidays.filter((valor) => valor === dezDaysList[i]);
     filtrado = Number(filtrado);
     if (filtrado > 0) liDay.classList.add('holiday');
 
-    filtrado = dezFridays.filter(valor => (valor === dezDaysList[i]));
+    filtrado = dezFridays.filter((valor) => valor === dezDaysList[i]);
     filtrado = Number(filtrado);
     if (filtrado > 0) liDay.classList.add('friday');
 
@@ -61,3 +72,14 @@ createButton(buttonsContainer, 'Feriados');
 //Exercicio 3
 // Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday".
 //  - É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)".
+const btnFeriados = document.getElementById('btn-holiday');
+btnFeriados.addEventListener('click', function () {
+  const holidays = document.getElementsByClassName('holiday');
+  for (let i = 0; i < holidays.length; i += 1) {
+    if (holidays[i].style.background === 'rgb(235, 204, 204)') {
+      holidays[i].style.background = 'rgb(238, 238, 238)';
+    } else {
+      holidays[i].style.background = 'rgb(235, 204, 204)';
+    }
+  }
+});
