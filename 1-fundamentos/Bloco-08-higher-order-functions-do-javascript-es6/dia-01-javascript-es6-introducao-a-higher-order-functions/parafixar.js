dd7245786bd95ff2ca5dccc77cde55b777c6cadb
Rubens Deola <rdeola@gmail.com>
2022-06-22 10:17:45 -0300

    para fixar 8.1

---
+++ 1-fundamentos/Bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/parafixar.js
@@ -0,0 +1,7 @@
+const wakeUp = () => 'Acordando!';
+const breakfest = () => 'Bora tomar café!';
+const sleepTime = () => 'Partiu dormir!';
+
+const doingThings = (action) => console.log(action);
+
+doingThings(breakfest());
